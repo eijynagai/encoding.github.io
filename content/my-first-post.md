e3caf31c1dcd0020303dcec88ed9d040fb92d430
eijynagai <eijynagai@gmail.com>
2021-01-15 19:30:41 +0900

    first commit

---
+++ content/my-first-post.md
@@ -0,0 +1,12 @@
+Title: Make a GitHub blog-page using Pelican[Python]
+Date: 2021-01-15 10:01
+Modified: 2021-01-15 12:30
+Category: learning
+Tags: learning, coding, blog, sphinx, pelican, GitHub, Python
+Slug: Make a blog at GitHub
+Authors: Eijy Nagai
+Summary: Host your blog at GitHub with Pelican.
+
+This is the *first post* from my Pelican blog. **YAY!**
+
+![Write here a good description for people who can't see the image]({static}/images/test.png)
---
+++ pelicanconf.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Eijy Nagai'
+SITENAME = 'encoding'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Japan'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+AUTHOR_FEED_RSS = 'feeds/%s.rss.xml'
+RSS_FEED_SUMMARY_ONLY = False
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+# Images
+STATIC_PATHS = ['images']
